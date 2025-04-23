0737334ca96c875858413da8fd381d891cf5157a
himeshpathai <himeshpathai2003@gmail.com>
2025-04-23 21:09:40 +0530

    change5

--- service-worker.js
+++ service-worker.js
@@ -13,8 +13,8 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
             return cache.addAll([
-                '/index.html',
-                '/offline.html',
+                '/IP-LAB3/index.html',
+                '/IP-LAB3/offline.html' 
             ]);
         })
     );
